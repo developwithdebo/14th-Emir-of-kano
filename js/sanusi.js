876444537177539b7509a12e3273e8761d4eff96
Adesanya Abdullateef Adebowale <debo@abdullateet.dev>
2024-01-01 22:24:23 +0100

    Add files via upload

---
+++ js/sanusi.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+  
+
+
+ $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+  
+
+
+   $(document).ready(function(){
+    $('.slider').slider();
+   });
+  
+
+   $(document).ready(function(){
+       $('.parallax-container').parallax();
+       
+   });
+  
+
+    $(document).ready(function(){
+   $('.tabs').tabs();
+    });
+   
+     $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
